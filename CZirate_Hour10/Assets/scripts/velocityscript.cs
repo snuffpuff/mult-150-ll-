e43ee74281d0135bb5a3ff4f0716a03343f54509
snuffpuff <sky.zirate@gmail.com>
2024-11-07 21:22:22 -0800

    chaos ball!

---
+++ CZirate_Hour10/Assets/scripts/velocityscript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityScript : MonoBehaviour
+{
+    public float startSpeed = 50f;
+    void Start()
+    {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = new Vector3(startSpeed, 0, startSpeed);
+    }
+}
+
